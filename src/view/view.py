899b2d76d5abc5b244dc1fbfddb5b832722985a9
mikecarti <mikecartibeats@gmail.com>
2023-07-14 05:39:22 -0400

    refactoring

--- src/view/view.py
+++ src/view/view.py
@@ -1,4 +1,12 @@
 class View:
     @staticmethod
-    def mock_process_answer(message):
-        return message
+    def process_answer(answer):
+        answer = answer.strip()
+        answer = View.remove_prefixes(answer)
+        return answer
+
+    @classmethod
+    def remove_prefixes(cls, answer):
+        if answer.startswith("AI: "):
+            return answer.replace("AI: ", "")
+        return answer
