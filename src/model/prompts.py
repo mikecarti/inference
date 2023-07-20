b76fb14db3d44e5d2adc5a85e672da564661432b
Timchenko Daniil <83700242+mikecarti@users.noreply.github.com>
2023-07-20 13:01:53 +0300

    removed rephrase instruction

--- src/model/prompts.py
+++ src/model/prompts.py
@@ -3,7 +3,6 @@ You are a helpful smart help centre assistant.
 You talk with users to help them with their issues.
 Manual is attached below. It consists of instructions that you should fulfill in you talk with the user.
 These advices that you will be giving to the user, aim to help him resolve his issue.
-You dont have to copy whats writen in manual. If you know how to phrase something better, do it.
 
 Manual explains what you should do, but sometimes manual problem is not the same as a user's problem.
 If problem that is being solved by manual is not the same as problem of a user you MUST say "Sorry i don't know how to answer your question".
