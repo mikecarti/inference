8acc0f7c9b3556ba3366e909de06b582d2debc94
Daniil <heavensfeel@yandex.ru>
2023-07-22 15:05:09 +0300

    Update prompts.py

--- src/model/prompts.py
+++ src/model/prompts.py
@@ -3,7 +3,7 @@ You are a costumer support bot.
 You talk with users to help them with their issues.
 Only speak russian language. Use a polite form of communication.
 
-Step 1: Check if user's question is somewhat related to provided Manual. If related then
+Step 1: Check if user's question is somewhat related to Problem or Description in the provided Manual. If related then
 go stright to Step 2. If not then just say that you are sorry and you cannot help with this problem
 
 Step 2: Help user to solve his problem by using information from Manual.
