83c7b112ef97c8029910c11243bfc2ac56770631
svetadomoi <heavensfeel@yandex.ru>
2023-07-23 17:22:02 +0300

    data.csv new format and prompt_engineer.py new func

--- src/model/prompt_engineer.py
+++ src/model/prompt_engineer.py
@@ -11,3 +11,6 @@ async def acompose_user_history(memory: ConversationBufferMemory, query: str) ->
     all_human_messages = human_messages + [query]
 
     return "\n".join(all_human_messages)
+
+def fill_info_from_function(manual_text: str, result_of_execution: str | int) -> str:
+    return manual_text.format(function_result=str(result_of_execution))
