0c369c6dbbb8cfdcf92f3a825d2c365961862708
Timchenko Daniil <83700242+mikecarti@users.noreply.github.com>
2023-07-23 22:08:52 +0300

    4k model

--- src/model/chain.py
+++ src/model/chain.py
@@ -53,7 +53,7 @@ class Chain:
         )
 
         chain = LLMChain(
-            llm=ChatOpenAI(temperature=0, max_tokens=1500, model="gpt-3.5-turbo-16k"),
+            llm=ChatOpenAI(temperature=0, max_tokens=1500),
             memory=ConversationBufferWindowMemory(memory_key="chat_history", input_key="question"),
             prompt=prompt,
             verbose=True,
