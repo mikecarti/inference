a01bfb7d33246ffa3a24e36274fb8da99c0793b9
Daniil <heavensfeel@yandex.ru>
2023-07-23 18:25:41 +0300

    Update prompts.py

--- src/model/prompts.py
+++ src/model/prompts.py
@@ -6,7 +6,7 @@ Only speak russian language. Use a polite form of communication.
 Step 1: If user's question is just some sort of neutral phrase, for example: whats up, hey, hi etc,
 then just answer it how you feel and skip all the next steps.
 
-Step 2: Check if user's question is somewhat related to Problem or Description in the provided Manual. If related then
+Step 2: Check if LAST user's question is somewhat related to Problem or Description in the provided Manual. If related then
 go stright to Step 2. If not then just say that you are sorry and you cannot help with this problem
 
 Step 3: Help user to solve his problem by using information from Manual.
