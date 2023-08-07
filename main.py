99c743075a521f09ccc127d622cd2a555bae4388
mikecarti <mikecartibeats@gmail.com>
2023-08-07 08:43:02 -0400

    fix welcome

--- main.py
+++ main.py
@@ -42,7 +42,8 @@ async def send_welcome(message: types.Message) -> None:
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm DeskHelp Bot!")
+    await message.reply("Добрый день!\nЯ сотрудник поддержки, наделенный "
+                        "искуственным интеллектом, как могу помочь вам сегодня?")
 
 
 @dp.message_handler(commands=['clear'])
