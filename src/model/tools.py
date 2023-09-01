79879fa21af6b99cae48d7b37334b76a7b91d29c
mikecarti <mikecartibeats@gmail.com>
2023-09-01 06:34:47 -0400

    fix return errors

--- src/model/tools.py
+++ src/model/tools.py
@@ -7,6 +7,7 @@ def return_with_name(func) -> (str, List[str]):
     :param func:
     :return: function name and list of output
     """
+
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         if type(result) != list and type(result) != tuple:
@@ -25,6 +26,26 @@ class ToolConstructor:
     def change_background_color(self, color_hex: str):
         return color_hex
 
+    @return_with_name
+    def cashback_balance(self, x):
+        return "5 рублей"
+
+    @return_with_name
+    def delivery_status(self, x):
+        return "Shipment is being delivered. Will come in 10 minutes"
+
+    @return_with_name
+    def wallet_linking(self, x):
+        return "Virtual wallet linked successfully"
+
+    @return_with_name
+    def document_status(self, x):
+        return "Documents are preparing"
+
+    @return_with_name
+    def refund_status(self, x):
+        return "Refund will be provided in 1 hour"
+
     def _construct_tools(self) -> List[Tool]:
         tools = [
             Tool(
@@ -35,28 +56,28 @@ class ToolConstructor:
             ),
             Tool(
                 name="Cashback-Balance",
-                func=lambda x: "5 рублей",
+                func=self.cashback_balance,
                 description="useful for when you need to answer questions about cashback balance of a user",
             ),
             Tool(
                 name="Delivery-Status",
-                func=lambda x: "Shipment is being delivered. Will come in 10 minutes",
+                func=self.delivery_status,
                 description="useful for when you need to answer questions about delivery status (where is the current "
                             "delivery, when will it come, etc)",
             ),
             Tool(
                 name="Virtual-Wallet",
-                func=lambda x: "Virtual wallet linked successfully",
+                func=self.wallet_linking,
                 description="useful for when you need to link virtual wallet to user's account"
             ),
             Tool(
                 name="Document-Status",
-                func=lambda x: "Documents are preparing",
+                func=self.document_status,
                 description="Check status of documents that were sent to user"
             ),
             Tool(
                 name="Refund-Status",
-                func=lambda x: "Refund will be provided in 1 hour",
+                func=self.refund_status,
                 description="Check status of refund for last good that user purchased from our service"
             )
         ]
